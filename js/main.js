1fe1e1ec5343f6b0362bc758e779fd27a46bd091
Justin Rodriguez <justinrodriguez@live.com>
2020-11-04 20:40:31 -0800

    added functionality to my labels

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var picker = datepicker("#date-acquired");
+picker.setMin(new Date());
+var ToDoItem = (function () {
+    function ToDoItem() {
+    }
+    return ToDoItem;
+}());
+window.onload = function () {
+    var addItem = document.getElementById("add");
+    addItem.onclick = productProcess;
+};
+function productProcess() {
+    if (isValid()) {
+        var list = getToDoItem();
+        displayToDoItem(list);
+    }
+}
+function isValid() {
+    return true;
+}
+function getToDoItem() {
+    var myList = new ToDoItem();
+    var groceryInput = getInputById("grocery");
+    myList.grocery = groceryInput.value;
+    var quantityInput = getInputById("quantity");
+    myList.quantity = parseInt(quantityInput.value);
+    var dateAcquiredInput = getInputById("date-acquired");
+    myList.dateAcquired = new Date(dateAcquiredInput.value);
+    var itemGrabbed = getInputById("grabbed-item");
+    myList.itemGrabbed = itemGrabbed.checked;
+    return myList;
+}
+function displayToDoItem(item) {
+    var groceryText = document.createElement("h3");
+    groceryText.innerText = item.grocery;
+    var quantityText = document.createElement("p");
+    quantityText.innerText = item.quantity.toString();
+    var itemDate = document.createElement("p");
+    itemDate.innerText = item.dateAcquired.toDateString();
+    var itemDiv = document.createElement("div");
+    if (item.itemGrabbed) {
+        itemDiv.classList.add("completed");
+    }
+    itemDiv.appendChild(groceryText);
+    itemDiv.appendChild(quantityText);
+    itemDiv.appendChild(itemDate);
+    if (item.itemGrabbed) {
+        var completeCapture = document.getElementById("itemBagged");
+        completeCapture.appendChild(itemDiv);
+    }
+    else {
+        var incompleteCapture = document.getElementById("itemWasNotBagged");
+        incompleteCapture.appendChild(itemDiv);
+    }
+}
+function getInputById(id) {
+    return document.getElementById(id);
+}
---
+++ ts/main.ts
@@ -0,0 +1,108 @@
+// @ts-ignore: Ignoring issue with js-datepicker lack of intellisense
+const picker = datepicker("#date-acquired");
+picker.setMin(new Date()); // set to today's date
+
+class ToDoItem{
+    grocery:string;
+    quantity:Number;
+    dateAcquired:Date;
+    itemGrabbed:boolean;
+
+    /*
+    constructor(desiredGrocery:string) {
+        this.grocery = desiredGrocery;
+    }
+    */
+}
+/*
+let item = new ToDoItem();
+item.grocery = "Testing";
+item.quantity = 1;
+item.dateAcquired = new Date(2020, 11, 4);
+item.isCompleted = false;
+*/
+
+window.onload = function() {
+    let addItem = document.getElementById("add");
+    addItem.onclick = productProcess;
+}
+
+function productProcess() {
+    if(isValid()) {
+        let list = getToDoItem();
+        displayToDoItem(list);
+    }    
+}
+/**
+ * Check form data is valid
+ */
+function isValid():boolean {
+    return true;
+}
+
+/**
+ * Get all input off form and wrap in a 
+ * ToDoItem object
+ */
+function getToDoItem():ToDoItem {
+
+    let myList = new ToDoItem();
+    
+    let groceryInput = getInputById("grocery");
+    myList.grocery = groceryInput.value;
+
+    let quantityInput = getInputById("quantity");
+    myList.quantity = parseInt(quantityInput.value);
+
+    let dateAcquiredInput = getInputById("date-acquired");
+    myList.dateAcquired = new Date(dateAcquiredInput.value);
+
+    let itemGrabbed = getInputById("grabbed-item");
+    myList.itemGrabbed = itemGrabbed.checked;
+
+    return myList;
+}
+
+/**
+ * Display given ToDoItem on the web page
+ */
+function displayToDoItem(item:ToDoItem):void {
+    let groceryText = document.createElement("h3");
+    groceryText.innerText = item.grocery;
+
+    let quantityText = document.createElement("p");
+    quantityText.innerText = item.quantity.toString();
+
+    let itemDate = document.createElement("p");
+    itemDate.innerText = item.dateAcquired.toDateString();
+
+    let itemDiv = document.createElement("div");
+        if(item.itemGrabbed) {
+            itemDiv.classList.add("completed");
+        }
+
+    itemDiv.appendChild(groceryText);
+    itemDiv.appendChild(quantityText);
+    itemDiv.appendChild(itemDate);
+
+    if(item.itemGrabbed) {
+        let completeCapture = document.getElementById("itemBagged");
+        completeCapture.appendChild(itemDiv);
+    }
+    else {
+        let incompleteCapture = document.getElementById("itemWasNotBagged");
+        incompleteCapture.appendChild(itemDiv);
+    }
+
+}
+
+/**
+ * 
+ * @param id getInputById is a shortcut for casting HTMLInputElement
+ */
+function getInputById(id:string):HTMLInputElement {
+    return <HTMLInputElement>document.getElementById(id);
+}
+
+// Task: Allow user to mark a ToDoItem as completed
+// Task: Store ToDoItems in webStorage
